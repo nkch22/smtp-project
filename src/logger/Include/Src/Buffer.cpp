e38c1c63e889d6c1a82c2e9ad18c8cae1f77129a
Artur-Milash <arturmilash@gmail.com>
2025-03-13 18:39:05 +0200

    remove space in logger output

---
+++ src/logger/Include/Src/Buffer.cpp
@@ -0,0 +1,62 @@
+#include "Buffer.h"
+
+// Buffer
+
+using namespace logger;
+
+Buffer::Buffer() : Buffer{""} {}
+Buffer::Buffer(const std::string& str) : m_real_buff{new std::string{str}} {}
+
+Buffer::~Buffer()
+{
+	delete m_real_buff;
+}
+
+std::string Buffer::get() const
+{
+	return *m_real_buff;
+}
+
+void Buffer::clear()
+{
+	m_real_buff->clear();
+}
+
+Buffer& Buffer::operator<<(const std::string& str)
+{
+	*m_real_buff += str;
+	return *this;
+}
+Buffer& Buffer::operator<<(const char* str)
+{
+	*m_real_buff += str;
+	return *this;
+}
+Buffer& Buffer::operator<<(const char str)
+{
+	*m_real_buff += str;
+	return *this;
+}
+
+Buffer& Buffer::operator<<(const int value)
+{
+	*m_real_buff += std::to_string(value);
+	return *this;
+}
+Buffer& Buffer::operator<<(const unsigned int value)
+{
+	*m_real_buff += std::to_string(value);
+	return *this;
+}
+Buffer& Buffer::operator<<(const double value)
+{
+	*m_real_buff += std::to_string(value);
+	return *this;
+}
+Buffer& Buffer::operator<<(const bool value)
+{
+	*m_real_buff += std::to_string((int)value);
+	return *this;
+}
+
+
